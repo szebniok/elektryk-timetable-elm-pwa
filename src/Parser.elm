538f07238a37d6d0fb2e51093c88b2100d8ba910
Konrad Bochnia <kbochnia@gmail.com>
2017-11-08 19:48:06 +0100

    Add new function that parses whole day

--- src/Parser.elm
+++ src/Parser.elm
@@ -6,6 +6,13 @@ lessonDecoder : Int -> Int -> Decoder (List (Maybe LessonRecord))
 lessonDecoder day l = 
   field "data" <| index day <| at [ "c_" ++ toString l, "cards" ] <| (list (nullable lessonRecordDecoder))
 
+allMessagesInADay : String -> Int -> List (Maybe (List (Maybe LessonRecord)))
+allMessagesInADay json day = 
+  let 
+    getLessons n = Result.toMaybe (decodeString (lessonDecoder day n) json)
+  in
+    List.map getLessons (List.range 1 9)
+
 type alias LessonRecord =
   { subject : List String
   , teacher : List String
