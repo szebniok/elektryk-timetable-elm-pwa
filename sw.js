62eb2ed25af9ba3e07c6310c46e44ac8a048663d
Konrad Bochnia <kbochnia@gmail.com>
2017-11-16 17:46:25 +0100

    Service worker will try to cache new data in the background

--- sw.js
+++ sw.js
@@ -18,4 +18,12 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(caches.match(event.request).then(function (response) {
     return response || fetch(event.request);
   }))
+
+  // fetch and cache new data in the backgrond so it can be used on next
+  // boot
+  event.waitUntil(caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    });
+  }));
 })
