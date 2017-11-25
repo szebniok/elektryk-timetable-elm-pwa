bd9757aad30b29ecad679dbf0d5ba0ac3f3526b9
Konrad Bochnia <kbochnia@gmail.com>
2017-11-25 22:40:36 +0100

    Move substitutions view to separate file

--- src/View.elm
+++ src/View.elm
@@ -3,6 +3,7 @@ module View exposing (rootView)
 import Html exposing (..)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
+import Substitutions.View
 import Timetable.View
 import TouchEvents
 import Types exposing (..)
@@ -23,57 +24,7 @@ page model =
             Timetable.View.root model
 
         SubstitutionsPage ->
-            substitutions model
-
-
-substitutions : Model -> Html Msg
-substitutions model =
-    div [ class "page" ]
-        [ button [ onClick FetchSubstitutions ] [ text "pobierz" ]
-        , table []
-            (List.map substitution model.substitutions)
-        , if model.online then
-            p [] [ text "Jestes online" ]
-          else
-            p [] [ text "Jestes offline" ]
-        ]
-
-
-substitution : Substitution -> Html Msg
-substitution sub =
-    case sub of
-        Substitution period class ( subject, teacher, classroom ) ( oldSubject, oldTeacher, oldClassroom ) ->
-            let
-                oldSubjectDisplay =
-                    Maybe.withDefault subject oldSubject
-
-                oldTeacherDisplay =
-                    Maybe.withDefault teacher oldTeacher
-
-                oldClassroomDisplay =
-                    Maybe.withDefault classroom oldClassroom
-            in
-            tr []
-                [ td [] [ text (toString period) ]
-                , td [] [ text class.name ]
-                , td []
-                    [ text oldSubjectDisplay.name
-                    , br [] []
-                    , text (oldTeacherDisplay.firstname ++ " " ++ oldTeacherDisplay.lastname)
-                    , br [] []
-                    , text oldClassroomDisplay.name
-                    ]
-                , td []
-                    [ text subject.name
-                    , br [] []
-                    , text (teacher.firstname ++ " " ++ teacher.lastname)
-                    , br [] []
-                    , text classroom.name
-                    ]
-                ]
-
-        _ ->
-            text "zredukowane"
+            Substitutions.View.root model
 
 
 navigation : Page -> Html Msg
